aa3b45f8c787512ad114f5a07f17639e4b4773f9
jiang_clan <736635982@qq.com>
2021-07-19 10:31:19 +0800

    fts

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,10 @@ if [ -z "$GITHUB_TOKEN" ]; then
   githubUrl=git@github.com:julie7366/my-blog.git
 else
   msg='来自github action的自动部署'
-  githubUrl=https://julie7366:${GITHUB_TOKEN}@github.com/julie7366/my-blog.git
+  githubUrl="https://julie7366:${GITHUB_TOKEN}@github.com/julie7366/my-blog.git"
   git config --global user.name "julie7366"
   git config --global user.email "736635982@qq.com"
-fi;
+fi
 
 git init;
 git add -A;
