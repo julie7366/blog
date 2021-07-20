3186671ac5b52c2f15e8b88f1224d065afce13ef
julie7366 <52814166+julie7366@users.noreply.github.com>
2021-07-20 17:20:03 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 # 确保脚本抛出遇到的错误
 set -e
 npm run build # 生成静态文件
-cd docs/.vuepress/dist; # 进入生成的文件夹
+cd docs/.vuepress/dist # 进入生成的文件夹
 # deploy to github
 echo 'julie7366.github.io/my-blog' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
-  msg="deploy";
-  githubUrl=git@github.com:julie7366/my-blog.git;
+  msg='deploy'
+  githubUrl=git@github.com:julie7366/my-blog.git
 else
   msg='来自github action的自动部署'
   githubUrl=https://julie7366:${GITHUB_TOKEN}@github.com/julie7366/my-blog.git
@@ -17,4 +17,4 @@ fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages; # 推送到github
+git push -f "$githubUrl" master:gh-pages; # 推送到github
