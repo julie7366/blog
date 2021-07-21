9990c1ca1145c1c96ce644ed588fddc2d617cb60
jiang_clan <736635982@qq.com>
2021-07-21 15:51:46 +0800

    改成v-doing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,8 @@ const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
-  // theme: 'vdoing', // 使用依赖包主题
-  theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "呢呢 Blog",
   description: '记录工作生活中美好的事，好事正在发生..',
