a08e35ddf7fa40592091318b2216bf54c3deb1e1
jiang_clan <736635982@qq.com>
2021-07-21 18:18:41 +0800

    fts

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,49 +1,49 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  {
-    text: '编程',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-    items: [
-      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      {
-        text: '前端',
-        items: [
-        	{ text: 'web Worker使用笔记', link: '/pages/7caa55/' },
-        ],
-      },
-      {
-        text: '博客搭建',
-        items: [
-          { text: '快速上手', link: '/pages/41f87d890d0a02af/' },
-        ],
-      }
-    ],
-  },
-  { text: '文集', link: '/writings/' },
-  { text: '文档', link: '/docs/',
-  	items: [
-      { text: '前端公用方法', link: '/pages/7a91be2d502346ce/' },
-    ]
-  },
-  { text: '关于', link: '/about/' },
-  {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    items: [
-      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-      { text: '友情链接', link: '/friends/' },
-    ],
-  },
-  {
-    text: '索引',
-    link: '/archives/',
-    items: [
-      { text: '分类', link: '/categories/' },
-      { text: '标签', link: '/tags/' },
-      { text: '归档', link: '/archives/' },
-    ],
-  },
+  // {
+  //   text: '编程',
+  //   link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+  //   items: [
+  //     // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+  //     {
+  //       text: '前端',
+  //       items: [
+  //       	{ text: 'web Worker使用笔记', link: '/pages/7caa55/' },
+  //       ],
+  //     },
+  //     {
+  //       text: '博客搭建',
+  //       items: [
+  //         { text: '快速上手', link: '/pages/41f87d890d0a02af/' },
+  //       ],
+  //     }
+  //   ],
+  // },
+  // { text: '文集', link: '/writings/' },
+  // { text: '文档', link: '/docs/',
+  // 	items: [
+  //     { text: '前端公用方法', link: '/pages/7a91be2d502346ce/' },
+  //   ]
+  // },
+  // { text: '关于', link: '/about/' },
+  // {
+  //   text: '收藏',
+  //   link: '/pages/beb6c0bd8a66cea6/',
+  //   items: [
+  //     { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+  //     { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+  //     { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+  //     { text: '友情链接', link: '/friends/' },
+  //   ],
+  // },
+  // {
+  //   text: '索引',
+  //   link: '/archives/',
+  //   items: [
+  //     { text: '分类', link: '/categories/' },
+  //     { text: '标签', link: '/tags/' },
+  //     { text: '归档', link: '/archives/' },
+  //   ],
+  // },
 ]
