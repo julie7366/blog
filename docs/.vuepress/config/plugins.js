b8f8db84b738ca38d1eb078ffc549a619dd935e0
jiang_clan <736635982@qq.com>
2021-07-22 11:33:33 +0800

    添加评论插件

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -78,26 +78,26 @@ module.exports = [
       hm: '6a9a49626834d99cc7648236def78eb5',
     },
   ],
-  // [
-  //   'vuepress-plugin-comment', // 评论
-  //   {
-  //     choosen: 'gitalk',
-  //     options: {
-  //       clientID: '758b2f530b693124c34a',
-  //       clientSecret: 'ebd70204af495efd17f6fbcb8aca5d49bd276af9',
-  //       repo: 'blog', // GitHub 仓库
-  //       owner: 'julie7366', // GitHub仓库所有者
-  //       admin: ['julie7366','Ostask'], // 对仓库有写权限的人
-  //       // distractionFreeMode: true,
-  //       pagerDirection: 'last', // 'first'正序 | 'last'倒序
-  //       id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-  //       title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-  //       labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-  //       body:
-  //         '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-  //     },
-  //   },
-  // ],
+  [
+    'vuepress-plugin-comment', // 评论
+    {
+      choosen: 'gitalk',
+      options: {
+        clientID: '758b2f530b693124c34a',
+        clientSecret: 'ebd70204af495efd17f6fbcb8aca5d49bd276af9',
+        repo: 'blog', // GitHub 仓库
+        owner: 'julie7366', // GitHub仓库所有者
+        admin: ['julie7366','Ostask'], // 对仓库有写权限的人
+        // distractionFreeMode: true,
+        pagerDirection: 'last', // 'first'正序 | 'last'倒序
+        id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
+        title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
+        labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+        body:
+          '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+      },
+    },
+  ],
   [
     '@vuepress/last-updated', // "上次更新"时间格式
     {
