cb1df47747283b2a633611f59bd22d149120e7f7
jiang_clan <736635982@qq.com>
2021-10-15 17:08:06 +0800

    添加友链

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -36,14 +36,14 @@ module.exports = [
       { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
       { text: '友情链接', link: '/friends/' },
     ],
-  },
-  {
-    text: '索引',
-    link: '/archives/',
-    items: [
-      { text: '分类', link: '/categories/' },
-      { text: '标签', link: '/tags/' },
-      { text: '归档', link: '/archives/' },
-    ],
-  },
+  }
+  // {
+  //   text: '索引',
+  //   link: '/archives/',
+  //   items: [
+  //     { text: '分类', link: '/categories/' },
+  //     { text: '标签', link: '/tags/' },
+  //     { text: '归档', link: '/archives/' },
+  //   ],
+  // },
 ]
